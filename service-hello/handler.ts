cb66fb9553d98bd3515c367682e7acb7c9bc5325
Makoto Nihei <nihemak@gmail.com>
2019-01-01 13:04:17 +0900

    Move handler.hello to service-hello

--- service-hello/handler.ts
+++ service-hello/handler.ts
@@ -4,7 +4,7 @@ export const hello: APIGatewayProxyHandler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message: 'Hello! Your function executed successfully!',
       input: event,
     }),
   };
